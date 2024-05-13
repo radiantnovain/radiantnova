b7126741decf24ed41f65409be07b99379bbfd41
riddhielsnerr2102 <riddhi@elsner.in>
2024-05-13 11:56:38 +0530

    header responsiveness

--- resources/views/theme.blade.php
+++ resources/views/theme.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>RadiantNova</title>
@@ -32,23 +32,76 @@
 </head>
 
 <header>
+
     <div class="logo-main">
         <div class="logo">RadiantNova </div><span class="dot"></span>
     </div>
+    <div class="bars" id="bar-button">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="bar-icon"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+            <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
+        </svg>
+    </div>
     <div class="search">
-      <input type="text" class="searchTerm" placeholder="What are you looking for?">
-      <button type="submit" class="searchButton">
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
-</svg>
-     </button>
-   </div>
+        <input type="text" class="searchTerm" placeholder="What are you looking for?">
+        <button type="submit" class="searchButton">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+            </svg>
+        </button>
+    </div>
     <div class="buttons">
         <button class="login">Login</button>
         <button class="signup">Signup</button>
 
     </div>
 </header>
+<div class="sidebar" id="sidebar">
+    <div class="close-btn" id="closeBtnForSidebar">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+            <path d="M0.75 23.249L23.25 0.749023" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M23.25 23.249L0.75 0.749023" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+    </div>
+    <div class="menus-sidebar">
+        <div class="sidebar-search">
+            <div class="search-2">
+                <input type="text" class="searchTerm" placeholder="Search...">
+                <button type="submit" class="searchButton">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+                    </svg>
+                </button>
+            </div>
+        </div>
+        <div class="sidebar-buttons">
+            <div class="buttons-2">
+                <button class="login">Login</button>
+                <button class="signup">Signup</button>
+
+            </div>
+        </div>
+        <div class="sidebar-pages">
+            <div class="sub-header-2">
+                <div class="sub-header-item active">Home</div>
+                <div class="sub-header-item">Anime Characters</div>
+                <div class="sub-header-item">Stands</div>
+                <div class="sub-header-item">All Products</div>
+                <div class="sub-header-item">Help Center</div>
+
+
+
+            </div>
+
+        </div>
+        <!-- <a href="#"><div class="menu-name-sidebar">HOME</div></a>
+    <a href="#"><div class="menu-name-sidebar">ABOUT</div></a>
+    <a href="#"><div class="menu-name-sidebar">SERVICES</div></a>
+    <a href="#"><div class="menu-name-sidebar">Gallery</div></a>
+    <a href="#"><div class="menu-name-sidebar">CONTACT US</div></a> -->
+
+
+    </div>
+</div>
 <div class="sub-header">
     <div class="sub-header-item active">Home</div>
     <div class="sub-header-item">Anime Characters</div>
@@ -56,9 +109,24 @@
     <div class="sub-header-item">All Products</div>
     <div class="sub-header-item">Help Center</div>
 
-    
+
 
 </div>
 
+<body></body>
+<script>
+    document.getElementById('bar-button').addEventListener('click', function() {
+        var targetBlock = document.getElementById('sidebar');
+        var targetBlock1 = document.getElementById('backgroundSidebar');
+        targetBlock.style.display = 'block';
+
+    });
+
+    document.getElementById('closeBtnForSidebar').addEventListener('click', function() {
+        var targetBlock = document.getElementById('sidebar');
+        var targetBlock1 = document.getElementById('backgroundSidebar');
+        targetBlock.style.display = 'none';
+    });
+</script>
 
 </html>
