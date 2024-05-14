0016d0258010b5a7c0bc3f206607c60002fe8f96
riddhielsnerr2102 <riddhi@elsner.in>
2024-05-14 14:05:50 +0530

    dynamic images

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,8 +2,9 @@
 @section('content')
 <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/home.css'); }} ">
 <div class="home-container">
-    <div class="hero"><div class="right-hero">
-            <img src="{{URL::asset('imgs/item1.png');}}" alt="">
+    <div class="hero">
+        <div class="right-hero">
+            <img id="carouselImg" src="{{URL::asset('imgs/item1.png');}}" alt="">
         </div>
         <div class="left-hero">
             <div class="first-line">DISCOUNT OFFERS ON</div>
@@ -11,12 +12,24 @@
             <div class="third-line">
                 Step into a realm where innovation meets imagination. Browse our curated selection of lifelike 3D items, crafted to elevate your digital experiences. Unleash your creativity today!
             </div>
-
-            <span class="shop-now-button">
-                SHOP NOW
-            </span>
+            <span class="shop-now-button">SHOP NOW</span>
         </div>
-        
     </div>
 </div>
+
+<script>
+// JavaScript to rotate images
+document.addEventListener("DOMContentLoaded", function() {
+    const images = ["item1.png", "item2.png", "item3.png","item4.png","item5.png"]; // Replace with your image URLs
+    const interval = 1500; // Change interval as desired (in milliseconds)
+    let index = 0;
+    
+    setInterval(function() {
+        index = (index + 1) % images.length;
+        document.getElementById("carouselImg").src = "{{URL::asset('imgs/')}}/" + images[index];
+    }, interval);
+});
+
+</script>
+
 @endsection
