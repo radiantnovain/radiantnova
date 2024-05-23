8eed3eb1a20bdb9527f795c8606cca64de713898
riddhielsnerr2102 <riddhi@elsner.in>
2024-05-23 18:48:17 +0530

    trending items

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,8 +18,9 @@
     </div>
     <div class="trending-title">
         <h2>Trending 3D Printed Products</h2>
-        <div class="line-div"></div>
-        <div class="line-div-2"></div>
+        <p class="subline">Discover the latest and most innovative 3D printed designs available today</p>
+        <!-- <div class="line-div"></div> -->
+        <!-- <div class="line-div-2"></div> -->
     </div>
 
 
@@ -28,87 +29,116 @@
     <div class="trending-items">
         <div class="item-slider">
             <div class="item">
-                <img src="https://picsum.photos/200" alt="3D Printed Product 1">
-                <div class="item-content">
-                    <h3>Product Name 1</h3>
-                    <p class="description">Description of the product goes here.</p>
-                    <p class="price">Price: $19.99</p>
-                    <p class="rating">Rating: <span>4.2</span></p>
+                <div class="item-2">
+
+                    <img class="product-img" src="https://picsum.photos/200" alt="3D Printed Product 1">
+                    <div class="item-content">
+                        <div class="first-line-product">
+                            <div class="product-heading">
+                                <h3>Product Name 1</h3>
+                            </div>
+                            <div class="add-to-cart-btn">
+                                Add to Cart
+                            </div>
+                        </div>
+                        <p class="description">Description will appear here for this product or item.</p>
+                        <p class="price">$19.99</p>
+                        <!-- <p class="rating">Rating: <span>4.2</span></p> -->
+                    </div>
+                </div>
+                <div class="lower-btn">
+                    Buy Now
                 </div>
             </div>
             <div class="item">
-                <img src="https://picsum.photos/200" alt="3D Printed Product 1">
-                <div class="item-content">
-                    <h3>Product Name 1</h3>
-                    <p class="description">Description of the product goes here.</p>
-                    <p class="price">Price: $19.99</p>
-                    <p class="rating">Rating: <span>4.2</span></p>
+                <div class="item-2">
+
+                    <img class="product-img" src="https://picsum.photos/200" alt="3D Printed Product 1">
+                    <div class="item-content">
+                        <div class="first-line-product">
+                            <div class="product-heading">
+                                <h3>Product Name 1</h3>
+                            </div>
+                            <div class="add-to-cart-btn">
+                                Add to Cart
+                            </div>
+                        </div>
+                        <p class="description">Description will appear here for this product or item.</p>
+                        <p class="price">$19.99</p>
+                        <!-- <p class="rating">Rating: <span>4.2</span></p> -->
+                    </div>
                 </div>
-            </div><div class="item">
-                <img src="https://picsum.photos/200" alt="3D Printed Product 1">
-                <div class="item-content">
-                    <h3>Product Name 1</h3>
-                    <p class="description">Description of the product goes here.</p>
-                    <p class="price">Price: $19.99</p>
-                    <p class="rating">Rating: <span>4.2</span></p>
-                </div>
-            </div><div class="item">
-                <img src="https://picsum.photos/200" alt="3D Printed Product 1">
-                <div class="item-content">
-                    <h3>Product Name 1</h3>
-                    <p class="description">Description of the product goes here.</p>
-                    <p class="price">Price: $19.99</p>
-                    <p class="rating">Rating: <span>4.2</span></p>
-                </div>
-            </div><div class="item">
-                <img src="https://picsum.photos/200" alt="3D Printed Product 1">
-                <div class="item-content">
-                    <h3>Product Name 1</h3>
-                    <p class="description">Description of the product goes here.</p>
-                    <p class="price">Price: $19.99</p>
-                    <p class="rating">Rating: <span>4.2</span></p>
-                </div>
-            </div><div class="item">
-                <img src="https://picsum.photos/200" alt="3D Printed Product 1">
-                <div class="item-content">
-                    <h3>Product Name 1</h3>
-                    <p class="description">Description of the product goes here.</p>
-                    <p class="price">Price: $19.99</p>
-                    <p class="rating">Rating: <span>4.2</span></p>
-                </div>
-            </div><div class="item">
-                <img src="https://picsum.photos/200" alt="3D Printed Product 1">
-                <div class="item-content">
-                    <h3>Product Name 1</h3>
-                    <p class="description">Description of the product goes here.</p>
-                    <p class="price">Price: $19.99</p>
-                    <p class="rating">Rating: <span>4.2</span></p>
-                </div>
-            </div><div class="item">
-                <img src="https://picsum.photos/200" alt="3D Printed Product 1">
-                <div class="item-content">
-                    <h3>Product Name 1</h3>
-                    <p class="description">Description of the product goes here.</p>
-                    <p class="price">Price: $19.99</p>
-                    <p class="rating">Rating: <span>4.2</span></p>
-                </div>
-            </div><div class="item">
-                <img src="https://picsum.photos/200" alt="3D Printed Product 1">
-                <div class="item-content">
-                    <h3>Product Name 1</h3>
-                    <p class="description">Description of the product goes here.</p>
-                    <p class="price">Price: $19.99</p>
-                    <p class="rating">Rating: <span>4.2</span></p>
-                </div>
-            </div><div class="item">
-                <img src="https://picsum.photos/200" alt="3D Printed Product 1">
-                <div class="item-content">
-                    <h3>Product Name 1</h3>
-                    <p class="description">Description of the product goes here.</p>
-                    <p class="price">Price: $19.99</p>
-                    <p class="rating">Rating: <span>4.2</span></p>
+                <div class="lower-btn">
+                    Buy Now
                 </div>
             </div>
+            <div class="item">
+                <div class="item-2">
+
+                    <img class="product-img" src="https://picsum.photos/200" alt="3D Printed Product 1">
+                    <div class="item-content">
+                        <div class="first-line-product">
+                            <div class="product-heading">
+                                <h3>Product Name 1</h3>
+                            </div>
+                            <div class="add-to-cart-btn">
+                                Add to Cart
+                            </div>
+                        </div>
+                        <p class="description">Description will appear here for this product or item.</p>
+                        <p class="price">$19.99</p>
+                        <!-- <p class="rating">Rating: <span>4.2</span></p> -->
+                    </div>
+                </div>
+                <div class="lower-btn">
+                    Buy Now
+                </div>
+            </div>
+            <div class="item">
+                <div class="item-2">
+
+                    <img class="product-img" src="https://picsum.photos/200" alt="3D Printed Product 1">
+                    <div class="item-content">
+                        <div class="first-line-product">
+                            <div class="product-heading">
+                                <h3>Product Name 1</h3>
+                            </div>
+                            <div class="add-to-cart-btn">
+                                Add to Cart
+                            </div>
+                        </div>
+                        <p class="description">Description will appear here for this product or item.</p>
+                        <p class="price">$19.99</p>
+                        <!-- <p class="rating">Rating: <span>4.2</span></p> -->
+                    </div>
+                </div>
+                <div class="lower-btn">
+                    Buy Now
+                </div>
+            </div>
+            <div class="item">
+                <div class="item-2">
+
+                    <img class="product-img" src="https://picsum.photos/200" alt="3D Printed Product 1">
+                    <div class="item-content">
+                        <div class="first-line-product">
+                            <div class="product-heading">
+                                <h3>Product Name 1</h3>
+                            </div>
+                            <div class="add-to-cart-btn">
+                                Add to Cart
+                            </div>
+                        </div>
+                        <p class="description">Description will appear here for this product or item.</p>
+                        <p class="price">$19.99</p>
+                        <!-- <p class="rating">Rating: <span>4.2</span></p> -->
+                    </div>
+                </div>
+                <div class="lower-btn">
+                    Buy Now
+                </div>
+            </div>
+
 
             <!-- Add more items here -->
         </div>
@@ -116,31 +146,17 @@
 </div>
 
 <script>
+    document.addEventListener("DOMContentLoaded", function() {
+        const images = ["item1.png", "item2.png", "item3.png", "item4.png", "item5.png"]; // Replace with your image URLs
+        const interval = 1500; // Change interval as desired (in milliseconds)
+        let index = 0;
 
-document.addEventListener("DOMContentLoaded", function() {
-    const images = ["item1.png", "item2.png", "item3.png","item4.png","item5.png"]; // Replace with your image URLs
-    const interval = 1500; // Change interval as desired (in milliseconds)
-    let index = 0;
-    
-    setInterval(function() {
-        index = (index + 1) % images.length;
-        document.getElementById("carouselImg").src = "{{URL::asset('imgs/')}}/" + images[index];
-    }, interval);
-});
+        setInterval(function() {
+            index = (index + 1) % images.length;
+            document.getElementById("carouselImg").src = "{{URL::asset('imgs/')}}/" + images[index];
+        }, interval);
+    });
 
     var lastScrollTop = 0;
-
-    window.addEventListener("scroll", function() {
-        var itemSlider = document.querySelector(".item-slider");
-        var homeContainer = document.querySelector(".home-container");
-        var st = window.pageYOffset || document.documentElement.scrollTop;
-
-        if (st > lastScrollTop) {
-            itemSlider.style.transform = "translateX(-10px)";
-        } else {
-            itemSlider.style.transform = "translateX(10px)";
-        }
-        lastScrollTop = st <= 0 ? 0 : st;
-    });
 </script>
 @endsection
